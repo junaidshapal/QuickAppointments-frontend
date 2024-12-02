7326116541db2ecc1b4dfe0216cff98321e34a8a
Junaid <mughaljunaidmughal136@gmail.com>
2024-12-02 20:21:50 +0500

    new

--- src/app/userManagement/users-management/users-management.component.ts
+++ src/app/userManagement/users-management/users-management.component.ts
@@ -10,7 +10,7 @@ export class UsersManagementComponent {
   users = [
     { id: 1, name: 'Junaid Mughal', email: 'junaid.mughal@example.com', role: 'Admin', status: 'Active' },
     { id: 2, name: 'Jamil Moughal', email: 'jamil.moughal@example.com', role: 'Doctor', status: 'Active' },
-    { id: 3, name: 'Michael Lee', email: 'michael.lee@example.com', role: 'Patient', status: 'Inactive' },
+    { id: 3, name: 'Touseef Mughal', email: 'michael.lee@example.com', role: 'Patient', status: 'Inactive' },
     { id: 4, name: 'Anna White', email: 'anna.white@example.com', role: 'Doctor', status: 'Active' },
     { id: 5, name: 'Chris Martin', email: 'chris.martin@example.com', role: 'Patient', status: 'Active' },
     { id: 6, name: 'Abdal Shahpal', email: 'abdal.aws@example.com', role: 'AWS Architect', status: 'Inactive' },
