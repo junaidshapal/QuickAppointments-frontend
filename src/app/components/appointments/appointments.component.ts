259f8da723155440ec16cc68b1aec14bdfa08b84
Junaid <mughaljunaidmughal136@gmail.com>
2024-12-10 20:56:59 +0500

    pagination controls

--- src/app/components/appointments/appointments.component.ts
+++ src/app/components/appointments/appointments.component.ts
@@ -48,17 +48,7 @@ export class AppointmentsComponent {
   }
 
   // Pagination methods
-  nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-    }
-  }
-
-  prevPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-    }
-  }
+  
 
   goToPage(page: number) {
     this.currentPage = page;
