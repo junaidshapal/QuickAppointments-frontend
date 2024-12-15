30ad45d539416a2bdd6b1292281ecb661f6e6ccf
husnain-aftab <husnainalii9875@gmail.com>
2024-12-15 16:23:28 +0500

    hello

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ export class NavbarComponent {
   faBars = faBars;
   faTimes = faTimes;
 
-  // Sidebar toggle state
+  //Sidebar toggle state nsjkjcbkwbwbc
   isSidebarOpen = true;
 
   // Navigation links
