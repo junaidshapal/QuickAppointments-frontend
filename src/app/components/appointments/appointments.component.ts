a1b2428c0d419cb6f579d43adf3220e1e96043b4
junaidshapal <mughaljunaidmughal136@gmail.com>
2024-12-22 19:01:55 +0500

    restored appointments

--- src/app/components/appointments/appointments.component.ts
+++ src/app/components/appointments/appointments.component.ts
@@ -7,7 +7,14 @@ import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./appointments.component.css']
 })
 export class AppointmentsComponent {
- 
+  appointments = [
+    { clientName: 'Junaid Shahpal', date: '2024-10-16', time: '10:00 AM', purpose: 'Business Meeting' },
+    { clientName: 'Juni dev', date: '2024-10-17', time: '11:30 AM', purpose: 'Consultation' },
+    { clientName: 'Abdul Khan', date: '2024-10-18', time: '02:00 PM', purpose: 'New Project' },
+    { clientName: 'Touseef Mughal', date: '2024-10-19', time: '09:00 AM', purpose: 'Interview' },
+    { clientName: 'Husnain Aftab Mughal', date: '2024-10-20', time: '01:00 PM', purpose: 'Consultation' },
+    { clientName: 'Abdal Mughal', date: '2024-10-24', time: '02:00 PM', purpose: 'Normal Checkup' },
+  ];
 
   //FontAwesome icons
   faEdit = faEdit;
