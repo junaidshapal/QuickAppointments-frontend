b623942de2e914033351d2210e2384faf66b434f
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-01-05 19:51:48 +0500

    ctr back

--- src/app/AuthComponents/login/login.component.ts
+++ src/app/AuthComponents/login/login.component.ts
@@ -12,7 +12,16 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   showPassword = false;
 
-  
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {}
 
