2e6770c8f43a15d428a7db3db1856b004d358db6
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-01-14 21:42:33 +0500

    some minor changes in login.ts

--- src/app/AuthComponents/login/login.component.ts
+++ src/app/AuthComponents/login/login.component.ts
@@ -8,9 +8,7 @@ import { AuthService } from '../../Auth/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  showPassword = false;
+
 
   constructor(
     private fb: FormBuilder,
@@ -35,15 +33,5 @@ export class LoginComponent implements OnInit {
       return;
     }
 
-    this.authService.login(this.loginForm.value).subscribe({
-      next: (response) => {
-        console.log('Login successful:', response);
-        this.router.navigate(['dashboard']);
-      },
-      error: (err) => {
-        console.error('Login failed:', err);
-        alert('Login failed! Please check your credentials.');
-      },
-    });
-  }
+   
 }
