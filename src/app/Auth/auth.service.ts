bbe749af212b822b4ece9a3f9d20704cbf4e6238
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-01-22 22:27:11 +0500

    jwtDEcode error

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import * as jwtDecode from 'jwt-decode';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
