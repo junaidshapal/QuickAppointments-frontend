81cc6ca0d90bcf2c8bfb871d882655791e915f49
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-01-29 23:40:04 +0500

    some minor

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -14,21 +14,7 @@ export class AuthService {
 
   constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {}
 
-  //Register User
-  register(user: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, user).pipe(
-      map((response) => {
-        console.log('Registration successful:', response);
-        return response;
-      }),
-      catchError((error) => {
-        console.error('Registration error:', error);
-        return throwError(() => error);
-      })
-    );
-  }
-
-  // Login User
+  Login User
   login(credentials: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, credentials).pipe(
       map((response: any) => {
