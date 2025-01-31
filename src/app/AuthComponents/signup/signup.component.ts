c98472567238568013d76047f80872b7a798b543
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-01-31 23:48:50 +0500

    minor changes

--- src/app/AuthComponents/signup/signup.component.ts
+++ src/app/AuthComponents/signup/signup.component.ts
@@ -20,14 +20,7 @@ export class SignupComponent {
     });
   }
 
-  togglePasswordVisibility() {
-    this.showPassword = !this.showPassword;
-  }
-
-  toggleConfirmPasswordVisibility() {
-    this.showConfirmPassword = !this.showConfirmPassword;
-  }
-
+  
   onSubmit(): void {
     if (this.registerForm.invalid) {
       alert('Please fill out the form correctly');
