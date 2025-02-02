a834972beab9a0411c9ae7c9d0f115b9c01775d4
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-02-02 20:23:42 +0500

    signup component back

---
+++ src/app/AuthComponents/signup/signup.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../Auth/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent {
+  registerForm: FormGroup;
+  showPassword = false;
+  showConfirmPassword = false;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      alert('Please fill out the form correctly');
+      return;
+    }
+
+    const { password, confirmPassword } = this.registerForm.value;
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    this.authService.register(this.registerForm.value).subscribe({
+      next: (response) => {
+        console.log('Registration successful:', response);
+        this.registerForm.reset();
+        alert('Signup successful! You can now log in.');
+      },
+      error: (err) => {
+        console.error('Registration failed:', err);
+        alert('Registration failed! Please try again.');
+      },
+    });
+  }
+}
