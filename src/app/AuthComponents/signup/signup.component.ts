9fc4b705c6ada8ff848b93a13d3840806f5d5960
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-02-03 20:53:47 +0500

    code cleaning

--- src/app/AuthComponents/signup/signup.component.ts
+++ src/app/AuthComponents/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../Auth/auth.service';
 
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
