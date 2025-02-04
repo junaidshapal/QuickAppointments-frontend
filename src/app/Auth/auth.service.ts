aa0c935b4b0fa9e7b01ade336f8d5a123d1eb03c
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-02-04 21:25:53 +0500

    login and logout in auth service

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -28,49 +28,7 @@ export class AuthService {
     );
   }
 
-  // Login User
-  login(credentials: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, credentials).pipe(
-      map((response: any) => {
-        console.log('Login successful:', response);
-        if (response.token) {
-          this.saveToken(response.token);
-        }
-        return response;
-      }),
-      catchError((err) => {
-        console.error('Login error:', err);
-        return throwError(() => err);
-      })
-    );
-  }
-
-  //Get Role from JWT
-  getRole(): string | null {
-    const token = this.getToken();
-    if (token) {
-      try {
-        const decodedToken = (jwtDecode as any)(token);
-        return decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'] || null;
-      } catch (error) {
-        console.error('Error decoding token:', error);
-        return null;
-      }
-    }
-    return null;
-  }
-
-  //Save Token to Local Storage
-  saveToken(token: string): void {
-    localStorage.setItem(this.tokenKey, token);
-  }
-
   
-  //Logout User
-  logout(): void {
-    localStorage.removeItem(this.tokenKey); // Clear JWT
-    console.log('User logged out successfully');
-  }
 
   //Check if User is Authenticated
   isAuthenticated(): boolean {
