76b7be5fd442e3c81685b1b759ab29eb0c2c84d5
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-02-05 20:28:37 +0500

    canActivate authGuard

--- src/app/Auth/role.guard.ts
+++ src/app/Auth/role.guard.ts
@@ -9,6 +9,15 @@ export class RoleGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const expectedRole = route.data['expectedRole'];
+    const userRole = this.authService.getRole();
+
+    if (userRole === expectedRole) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
 }
