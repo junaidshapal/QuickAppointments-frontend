d3b3ee562b267ac35fc334896587af835ebb2721
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-02-06 22:10:20 +0500

    removing the role guard

--- src/app/Auth/role.guard.ts
+++ src/app/Auth/role.guard.ts
@@ -9,15 +9,5 @@ export class RoleGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-    const expectedRole = route.data['expectedRole'];
-    const userRole = this.authService.getRole();
-
-    if (userRole === expectedRole) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
-  }
+  
 }
