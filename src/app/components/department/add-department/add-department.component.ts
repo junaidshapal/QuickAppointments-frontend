d5127ba8085ced2cf9a7c2e6a3fdbd168328e99a
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-02-25 22:07:27 +0500

    add department ts file

--- src/app/components/department/add-department/add-department.component.ts
+++ src/app/components/department/add-department/add-department.component.ts
@@ -8,13 +8,5 @@ import { Department } from '../../../models/department.model';
 })
 
 export class AddDepartmentComponent {
-  department: Department = { id: 0, name: '', description: '' };
-
-  constructor(private departmentService: DepartmentService) {}
-
-  addDepartment() {
-    this.departmentService.addDepartment(this.department).subscribe(() => {
-      alert('Department added successfully');
-    });
-  }
+  
 }
