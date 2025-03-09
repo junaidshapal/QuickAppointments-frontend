dc8aa380d503f513dc3a81cc019ea300308af1f1
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-03-09 23:37:32 +0500

    Signup working fine

--- src/app/AuthComponents/signup/signup.component.ts
+++ src/app/AuthComponents/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../Auth/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,16 +9,67 @@ import { AuthService } from '../../Auth/auth.service';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
+// export class SignupComponent {
+//   registerForm: FormGroup;
+//   showPassword = false;
+//   showConfirmPassword = false;
+
+//   constructor(private fb: FormBuilder, private authService: AuthService) {
+//     this.registerForm = this.fb.group({
+//       email: ['', [Validators.required, Validators.email]],
+//       password: ['', [Validators.required, Validators.minLength(6)]],
+//       confirmPassword: ['', Validators.required],
+//     });
+//   }
+
+//   togglePasswordVisibility() {
+//     this.showPassword = !this.showPassword;
+//   }
+
+//   toggleConfirmPasswordVisibility() {
+//     this.showConfirmPassword = !this.showConfirmPassword;
+//   }
+
+//   onSubmit(): void {
+//     if (this.registerForm.invalid) {
+//       alert('Please fill out the form correctly');
+//       return;
+//     }
+
+//     const { password, confirmPassword } = this.registerForm.value;
+
+//     if (password !== confirmPassword) {
+//       alert('Passwords does not match');
+//       return;
+//     }
+
+//     this.authService.register(this.registerForm.value).subscribe({
+//       next: (response) => {
+//         console.log('Registration successful:', response);
+//         this.registerForm.reset();
+//         alert('Signup successful! You can now log in.');
+//       },
+//       error: (err) => {
+//         console.error('Registration failed:', err);
+//         alert('Registration failed! Please try again later.');
+//       },
+//     });
+//   }
+// }
 export class SignupComponent {
   registerForm: FormGroup;
   showPassword = false;
   showConfirmPassword = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required],
+      role: ['', Validators.required],  // New Role Selection
+      specialty: ['']  // Specialty (Required only for Doctors)
     });
   }
 
@@ -28,25 +80,36 @@ export class SignupComponent {
   toggleConfirmPasswordVisibility() {
     this.showConfirmPassword = !this.showConfirmPassword;
   }
-
+  // Handle Role Change
+  onRoleChange(event: Event): void {
+    const selectedRole = (event.target as HTMLSelectElement).value;
+    if (selectedRole === 'Doctor') {
+      this.registerForm.addControl('specialty', this.fb.control('', Validators.required));
+    } else {
+      this.registerForm.removeControl('specialty');
+    }
+  }
   onSubmit(): void {
     if (this.registerForm.invalid) {
       alert('Please fill out the form correctly');
       return;
     }
-
-    const { password, confirmPassword } = this.registerForm.value;
-
+    const { password, confirmPassword, role, specialty } = this.registerForm.value;
     if (password !== confirmPassword) {
-      alert('Passwords does not match');
+      alert('Passwords do not match');
       return;
     }
-
+    if (role === 'Doctor' && !specialty) {
+      alert('Doctors must enter a specialty.');
+      return;
+    }
+    
     this.authService.register(this.registerForm.value).subscribe({
       next: (response) => {
         console.log('Registration successful:', response);
         this.registerForm.reset();
         alert('Signup successful! You can now log in.');
+        this.router.navigate(['/login']);
       },
       error: (err) => {
         console.error('Registration failed:', err);
@@ -55,3 +118,4 @@ export class SignupComponent {
     });
   }
 }
+
