37c09f6e2096e816dca5c889bfa87c74a8caba22
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-03-12 23:29:02 +0500

    my-schedules

--- src/app/components/my-schedules/my-schedules.component.ts
+++ src/app/components/my-schedules/my-schedules.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AvailabilityService } from '../../services/availability.service';
 import { Availability } from '../../models/availability.model';
+import { AuthService } from '../../Auth/auth.service';
 
 @Component({
   selector: 'app-my-schedules',
@@ -8,13 +9,40 @@ import { Availability } from '../../models/availability.model';
 })
 export class MySchedulesComponent implements OnInit {
   availabilities: Availability[] = [];
+  doctorId: string = ''; // Updated to GUID
 
-  constructor(private availabilityService: AvailabilityService) {}
+  constructor(
+    private availabilityService: AvailabilityService,
+    private authService: AuthService // Inject AuthService to get logged-in doctor
+  ) {}
 
   ngOnInit() {
-    const doctorId = 1; // Assume logged-in doctor
-    this.availabilityService.getAvailabilityByDoctor(doctorId).subscribe(data => {
-      this.availabilities = data;
+    this.getLoggedInDoctorId();
+  }
+
+  getLoggedInDoctorId() {
+    const userId = this.authService.getUserId(); // Fetch user ID from JWT Token
+    if (userId) {
+      this.doctorId = userId;
+    } else {
+      console.error('Doctor ID not found in JWT.');
+    }
+    console.log('Logged-in Doctor ID:', this.doctorId);
+
+    if (this.doctorId) {
+      this.fetchAvailability();
+    } else {
+      console.error('Doctor ID not found in JWT.');
+    }
+  }
+
+  fetchAvailability() {
+    this.availabilityService.getAvailabilityByDoctor(this.doctorId).subscribe({
+      next: (data) => {
+        console.log('Fetched availability:', data);
+        this.availabilities = data;
+      },
+      error: (err) => console.error('Error fetching availability:', err),
     });
   }
 }
