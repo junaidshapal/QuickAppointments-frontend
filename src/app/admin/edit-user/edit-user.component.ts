2cbc190888c7fe0102bcbf137610938bbbc7013b
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-03-17 22:05:23 +0500

    edit user by admin

---
+++ src/app/admin/edit-user/edit-user.component.ts
@@ -0,0 +1,31 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AdminService } from '../services/admin.service';
+
+@Component({
+  selector: 'app-edit-user',
+  templateUrl: './edit-user.component.html',
+  styleUrls: ['./edit-user.component.css']
+})
+export class EditUserComponent {
+  updatedUser: any = {};
+
+  constructor(
+    public dialogRef: MatDialogRef<EditUserComponent>,
+    @Inject(MAT_DIALOG_DATA) public user: any,
+    private adminService: AdminService
+  ) {
+    this.updatedUser = { ...user }; //Copy existing user data
+  }
+
+  saveChanges() {
+    this.adminService.updateUser(this.updatedUser.id, this.updatedUser).subscribe(() => {
+      alert('User Updated Successfully');
+      this.dialogRef.close(true); //Close modal and reload users
+    });
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+}
