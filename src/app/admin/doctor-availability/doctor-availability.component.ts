727ba377230ba8a488f05c62379c3bb82659a5eb
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-04-10 22:01:06 +0500

    content undo

---
+++ src/app/admin/doctor-availability/doctor-availability.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { Availability } from '../../models/availability.model';
+import { AppointmentService } from '../../services/appointment.service';
+import { AvailabilityService } from '../../services/availability.service';
+
+@Component({
+  selector: 'app-doctor-availability',
+  templateUrl: './doctor-availability.component.html'
+})
+export class DoctorAvailabilityComponent implements OnInit {
+  @Input() doctorId!: string;
+  availabilities: Availability[] = [];
+  loading: boolean = true;
+
+  constructor(private adminService: AdminService,
+    private availabilityService:AvailabilityService,
+    private appointmentService: AppointmentService) {}
+
+  ngOnInit() {
+    this.loadAvailability();
+  }
+
+  loadAvailability() {
+    this.availabilityService.getAvailabilityByDoctor(this.doctorId).subscribe({
+      next: (data) => {
+        debugger
+        this.availabilities = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching availability:', err);
+        this.loading = false;
+      }
+    });
+  }
+
+  cancelAppointment(availabilityId: number) {
+    if (confirm('Are you sure you want to cancel this availability?')) {
+      this.adminService.deleteDoctorAvailability(availabilityId).subscribe({
+        next: () => {
+          alert('Availability canceled.');
+          this.loadAvailability();
+        },
+        error: (err) => console.error('Error canceling availability:', err)
+      });
+    }
+  }
+}
